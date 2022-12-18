979c00d38d30b2392a3719be71fa9b092b629733
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-18 10:07:03 +0530

    Check Even or Odd

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicCoreProgram
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            //VowelOrConstant.CheckCharacter();
+            //Swap.SwapNumbers();
+            // QuotientAndRemainder.FindQuotientAndRemainder();
+            //LargestNumber.FindTheLargestNumber();
+            EvenOrOdd.FindEvenOrOdd();
+        }
+    }
+}
